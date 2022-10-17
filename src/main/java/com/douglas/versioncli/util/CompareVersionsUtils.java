81f4eb692d5a74cc90a7aa98a5333194f5474b4e
Douglas Santos <douglassantoswork@gmail.com>
2022-10-17 17:58:27 +0100

    add comments to util class

--- src/main/java/com/douglas/versioncli/util/CompareVersionsUtils.java
+++ src/main/java/com/douglas/versioncli/util/CompareVersionsUtils.java
@@ -9,7 +9,10 @@ public class CompareVersionsUtils {
         String[] version1String = version1.split(SPLIT_REG_EX);
         String[] version2String = version2.split(SPLIT_REG_EX);
 
+        //check which version has the higher length
         int maxSize = Math.max(version1String.length, version2String.length);
+
+        //compare each major.minor.etc numbers
         for(int n = 0; n < maxSize; n++){
             int v1 = n < version1String.length ? Integer.parseInt(version1String[n]) : 0;
             int v2 = n < version2String.length ? Integer.parseInt(version2String[n]) : 0;
